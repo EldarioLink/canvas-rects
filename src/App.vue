15f61e7d0307e13e718bbdf9300bfce2b7ebc56c
EldarioLink <catshomee@gmail.com>
2022-07-28 17:04:34 +0300

    feat: add css to btns

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <div id="app">
-    <div>
-      <button @click="addRect">Добавить</button>
-      <button @click="deleteRects">Очистить</button>
-      <button @click="saveLocal">Сохранить</button>
-      <button @click="restoreLocal">Восстановить</button>
+    <div class="menu_btns">
+      <button class="button-13" role="button" @click="addRect">Добавить</button>
+
+      <button class="button-13" role="button" @click="deleteRects">
+        Очистить
+      </button>
+      <button class="button-13" role="button" @click="saveLocal">
+        Сохранить
+      </button>
+      <button class="button-13" role="button" @click="restoreLocal">
+        Восстановить
+      </button>
     </div>
 
     <v-stage ref="stage" :config="stageSize">
@@ -325,5 +332,42 @@ body {
 
 #menu button:hover {
   background-color: lightgray;
+}
+
+.menu_btns {
+  display: flex;
+  justify-content: space-around;
+}
+.button-13 {
+  background-color: #fff;
+  border: 1px solid #d5d9d9;
+  border-radius: 8px;
+  box-shadow: rgba(213, 217, 217, 0.5) 0 2px 5px 0;
+  box-sizing: border-box;
+  color: #0f1111;
+  cursor: pointer;
+  display: inline-block;
+  font-family: "Amazon Ember", sans-serif;
+  font-size: 13px;
+  line-height: 29px;
+  padding: 0 10px 0 11px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: middle;
+  width: 100px;
+}
+
+.button-13:hover {
+  background-color: #f7fafa;
+}
+
+.button-13:focus {
+  border-color: #008296;
+  box-shadow: rgba(213, 217, 217, 0.5) 0 2px 5px 0;
+  outline: 0;
 }
 </style>
